70d9852a68dcf129d63933b2b45b64c0a60f19f4
roronan13 <ronan.pothier@gmail.com>
2026-05-21 17:30:22 +0200

    oui

--- ex2/strategies.py
+++ ex2/strategies.py
@@ -1,15 +1,42 @@
 from abc import ABC, abstractmethod
+from ex0.creatures import Creature
+from ex1 import capabilities
 
 
 class BattleStrategy(ABC):
     @abstractmethod
-    def act(self) -> None:
-        pass
+    def act(self) -> str:
+        return ("No action available")
 
     @abstractmethod
     def is_valid(self) -> bool:
         return False
-    
 
-class 
+
+class NormalStrategy(BattleStrategy):
+    def act(self) -> str:
+        print(self.attack())
+
+    def is_valid(self) -> bool:
+        return (isinstance(self, Creature))
+
+
+class AggressiveStrategy(BattleStrategy):
+    def act(self) -> str:
+        print(self.transform())
+        print(self.attack())
+        print(self.revert())
+
+    def is_valid(self) -> bool:
+        return (isinstance(self, capabilities.TransformCapability))
+
+
+class DefensiveStrategy(BattleStrategy):
+    def act(self) -> str:
+        print(self.attack())
+        print(self.heal())
+
+    def is_valid(self) -> bool:
+        return (isinstance(self, capabilities.HealCapability))
+
 
